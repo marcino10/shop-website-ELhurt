a0b0377d8dc6f6d34ab4d8ac46d977747b256069
marcino10 <marcino10.krol@gmail.com>
2023-11-07 10:02:32 +0100

    done slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const mainSliderBtns = document.querySelectorAll('.slider-btn');
 const mainSlider = document.querySelector('.main-slider');
 const mainSlides = document.querySelectorAll('.main-slide');
-const mainSlidesText = document.querySelectorAll('.main-slide-text');
+const mainSlidesText = document.querySelectorAll('.slide-content-wrapper');
+
+console.log(mainSlidesText);
 
 const changeSlider = () => {
 	mainSlides.forEach(slide => slide.classList.toggle('hide'));
