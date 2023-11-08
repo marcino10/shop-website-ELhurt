0567eb435892ce75fe9164566c1c30c9f1468d70
marcino10 <marcino10.krol@gmail.com>
2023-11-08 14:24:38 +0100

    cleaned code + improved header and top-nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 const mainSliderBtns = document.querySelectorAll('.slider-btn');
-const mainSlider = document.querySelector('.main-slider');
 const mainSlides = document.querySelectorAll('.main-slide');
 const mainSlidesText = document.querySelectorAll('.slide-content-wrapper');
-
-console.log(mainSlidesText);
 
 const changeSlider = () => {
 	mainSlides.forEach(slide => slide.classList.toggle('hide'));
